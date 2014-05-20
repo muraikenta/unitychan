c97233a9444a64a6acfd954fe97cac8a11ec9b3f
muraikenta <muraikenta0507@gmail.com>
2014-05-20 14:45:29 +0900

    initial

---
+++ Assets/Jump.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var motion = GetComponent(Animator);
+	var state : AnimatorStateInfo = motion.GetCurrentAnimatorStateInfo(0);
+
+	if(Input.GetKeyDown("space")){
+		motion.SetBool("Jump", true);
+	}
+
+	if(state.IsName("Locomotion.JUMP00")){
+		motion.SetBool("Jump", false);
+	}
+
+}
